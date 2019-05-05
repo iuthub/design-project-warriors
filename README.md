31376425b2a2bc84c4db0c5bcf004b689a2179a9
unknown <m.sharipov2@student.inha.uz>
2019-05-05 14:04:14 +0500

    Revert "M#version"
    This reverts commit 1021f35cce9f1a402e8a078482b977632dfd4329.

--- README.md
+++ README.md
@@ -8,10 +8,6 @@ Mirsoip Adilov u1710208
 
 
 Find `ip_design_project.pdf` file in this folder, and refer to all the instructions given there. 
-Mirsoip Adilov
-Abdukuddus Azizov 
-Mukhammadalo Sharipov
-Bilol Sobirov
 
 You have to submit your project into this repository before 05.05.2019 (midnight).
 
--- project1/project1/bootstrap/app.php
+++ project1/project1/bootstrap/app.php
@@ -51,15 +51,5 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
 
 return $app;
